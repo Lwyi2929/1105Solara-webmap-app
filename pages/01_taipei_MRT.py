df7835756e03c7d7e35141f1227661f41abe836f
Lwyi2929 <lwyi2929@gmail.com>
2025-11-10 16:51:18 +0000

    modified:   pages/01_taipei_MRT.py

--- pages/01_taipei_MRT.py
+++ pages/01_taipei_MRT.py
@@ -1,43 +1,25 @@
-import os
-import json
+
 import solara
-import leafmap
+import leafmap.maplibregl as leafmap
 
 def create_map():
-    geojson_path = "/workspaces/1105Solara-webmap-app/data/tpeMRT.geojson"
-
-    # 讀取 GeoJSON
-    if os.path.exists(geojson_path):
-        with open(geojson_path, "r", encoding="utf-8") as f:
-            geojson_data = json.load(f)
-    else:
-        geojson_data = None
-
-    # 建立地圖
+   
     m = leafmap.Map(
-        projection="globe",
+        center=[121.55555, 25.08722],
+        zoom=16,
+        pitch=60,
+        bearing=-17,
+        style="positron",
         height="750px",
-        center=[25.05, 121.5],
-        zoom=10,
         sidebar_visible=True,
     )
     m.add_basemap("CartoDB.DarkMatter")
-
-    # 加入 GeoJSON
-    if geojson_data:
-        m.add_geojson(
-            geojson_data,
-            style={"color": "#ffffff", "weight": 2, "opacity": 1},
-            tooltip=True,
-            layer_name="MRT Routes",
-        )
-    else:
-        m.add_text("⚠️ 找不到 data/tpeMRT.geojson", position="topright")
+    
+    m.add_geojson("tpeMRT.geojson", name="routes")
 
     return m
 
 @solara.component
 def Page():
     m = create_map()
-    html = m.to_html()  # Leafmap 0.57.2 不支援 to_solara()
-    return solara.HTML(tag="div", unsafe_innerHTML=html)
+    return m.to_solara()
