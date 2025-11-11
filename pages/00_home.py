8d24c85cc3d5840615255822d7e46521f81b8a12
Lwyi2929 <lwyi2929@gmail.com>
2025-11-11 08:11:32 +0000

    modified:   pages/00_home.py

--- pages/00_home.py
+++ pages/00_home.py
@@ -23,12 +23,13 @@ def create_map():
 
 @solara.component
 def Page():
-    with solara.Column(align="center"):
-        markdown = """
-        ## 台北GIS儀表板
-        台北GIS儀表板是一個以 **Solara** 為基礎開發的互動式三維地圖應用程式，整合 **Leafmap** 與 **MapLibre GL** 技術，能即時呈現台北地區的地理空間資訊。  
-        """
     m = create_map()
-    solara.Markdown(markdown)
-    return m.to_solara()
+    markdown = """
+    ## 台北GIS儀表板
+    台北GIS儀表板是一個以 **Solara** 為基礎開發的互動式三維地圖應用程式，整合 **Leafmap** 與 **MapLibre GL** 技術，能即時呈現台北地區的地理空間資訊。
+    """
+
+    with solara.Column(align="center"):
+        solara.Markdown(markdown)
+        solara.FigurePlotly(m.to_solara())
     
