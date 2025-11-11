cdaa8b0dd7ad6689e8f87c66432b38e42f241657
Lwyi2929 <lwyi2929@gmail.com>
2025-11-11 01:45:21 +0000

    modified:   pages/00_home.py

--- pages/00_home.py
+++ pages/00_home.py
@@ -1,13 +1,15 @@
 import solara
-import leafmap.maplibregl as leafmap
+
 
 @solara.component
 def Page():
     with solara.Column(align="center"):
         markdown = """
-        ## 🗺️ 台北GIS儀表板
-        歡迎來到台北GIS儀表板！這個應用程式展示了台北市的地理資訊系統功能，讓您可以探索城市的各種地理數據和地圖視覺化。
+        ## 3D Mapping with Leafmap and MapLibre
+        This is a Solara template for a 3D mapping application using Leafmap and MapLibre. Click on the menu above to see the different examples.
+        <br>
+        Source code: <https://github.com/opengeos/solara-maplibre>
+        ![image](https://github.com/user-attachments/assets/efc9e43b-99c0-40b4-af08-4971e8b96919)
         """
-        
 
-    solara.Markdown(markdown)
+        solara.Markdown(markdown)
